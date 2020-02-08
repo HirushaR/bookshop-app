5e290958f2f33d79466f30c321d19c87031267ba
HirushaR <hirusharandunu11@gmail.com>
2020-02-08 16:08:31 +0530

    show case

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'models/book.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,12 +14,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class BooksHome extends StatefulWidget {
-  @override
-  _BooksHomeState createState() => _BooksHomeState();
-}
-
-class _BooksHomeState extends State<BooksHome> {
+class BooksHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,6 +23,8 @@ class _BooksHomeState extends State<BooksHome> {
           scrollDirection: Axis.vertical,
           children: <Widget>[
             CustomTitleBar(),
+            HorizontalListView(),
+
           ],
         ),
       ),
@@ -34,16 +32,11 @@ class _BooksHomeState extends State<BooksHome> {
   }
 }
 
-class CustomTitleBar extends StatefulWidget {
-  @override
-  _CustomTitleBarState createState() => _CustomTitleBarState();
-}
-
-class _CustomTitleBarState extends State<CustomTitleBar> {
+class CustomTitleBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 20,vertical: 35),
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 35),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
@@ -55,17 +48,109 @@ class _CustomTitleBarState extends State<CustomTitleBar> {
           SizedBox(
             width: 15,
           ),
-          Expanded(child: Text(
-            'Bookshop',
-            style: TextStyle(
-              fontSize: 30,
-
-            ),
-          ))
+          Expanded(
+            child: Text("BooksApp",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontFamily: "lovelo",
+                )),
+          )
         ],
       ),
     );
   }
 }
 
+class HorizontalListView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.bottomCenter,
+          end: Alignment.topCenter,
+          colors: [Color(0xfff8f8f8), Colors.white],
+        ),
+        border: Border(bottom: BorderSide(color: Color(0xfff0f0f0), width: 2)),
+      ),
+      padding: EdgeInsets.only(left: 20.0, right: 20.0, bottom: 20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            height: 250,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: booklist.books.length,
+              itemBuilder: (BuildContext context, int i) {
+                if (booklist.books[i].franchise) {
+                  return ShowCase(
+                    wideImage: booklist.books[i].wideImage,
+                    bookObject: booklist.books[i],
+                  );
+                } else {
+                  return Container();
+                }
+              },
+            ),
+          ),
+          SizedBox(
+            height: 25.0,
+          ),
+          Text(
+            "BEST SELLERS",
+            style: TextStyle(letterSpacing: 1.5, color: Colors.black54),
+          ),
+          Divider(
+            color: Colors.black54,
+          ),
+          Text(
+            "Novels with a popular franchise",
+            style: TextStyle(
+                color: Colors.black87,
+                fontWeight: FontWeight.w700,
+                fontSize: 30),
+            softWrap: true,
+          ),
+        ],
+      ),
+    );
+  }
+}
 
+class ShowCase extends StatelessWidget {
+  const ShowCase({
+    @required this.wideImage,
+    @required this.bookObject,
+  });
+
+  final String wideImage;
+  final Book bookObject;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        /* /* leaving this null, checkout the master branch to get its code */ */
+      },
+      child: Row(
+        children: <Widget>[
+          Container(
+            height: 250,
+            width: 350,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.network(wideImage, fit: BoxFit.cover),
+            ),
+          ),
+          SizedBox(
+            width: 20,
+          )
+        ],
+      ),
+    );
+  }
+}
