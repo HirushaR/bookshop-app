f2f0329ff9e756475781ffade5c305c6468541a1
HirushaR <hirusharandunu11@gmail.com>
2020-02-08 16:45:59 +0530

    edit bodysection

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class BooksHome extends StatelessWidget {
           children: <Widget>[
             CustomTitleBar(),
             HorizontalListView(),
+            HotList(),
 
           ],
         ),
@@ -154,3 +155,156 @@ class ShowCase extends StatelessWidget {
     );
   }
 }
+
+class HotList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            colors: [Color(0xfff8f8f8), Colors.white],
+          begin: Alignment.bottomCenter,
+          end: Alignment.topCenter,
+        ),
+        border: Border(bottom: BorderSide(
+          color: Color(0xfff0f0f0),
+          width: 2,
+        ))
+      ),
+      padding: EdgeInsets.only(left: 20, right: 20, top: 35, bottom: 15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(padding: EdgeInsets.only(bottom: 20.0),
+          child: Text(
+            "New Book Hot List",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 30.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          ),
+          Container(
+            height: 320,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: booklist.books.length,
+                itemBuilder: (BuildContext context, int i){
+                  String title = booklist.books[i].title.length > 17
+                      ? booklist.books[i].title.substring(0, 15) + "..."
+                      : booklist.books[i].title;
+
+                  if (booklist.books[i].franchise)
+                    {
+                      return Container();
+                    }else{
+                    return ShowBooks(
+                      bookCoverImg: booklist.books[i].cover,
+                      author: booklist.books[i].author,
+                      title: title,
+                      price: booklist.books[i].price,
+                    );
+                  }
+                }
+            ),
+          ),
+
+        ],
+      ),
+    );
+  }
+}
+
+class ShowBooks extends StatelessWidget {
+
+  const ShowBooks({
+    @required this.bookCoverImg,
+    @required this.author,
+    @required this.title,
+    @required this.price,
+    @required this.bookobject,
+
+});
+
+  final String bookCoverImg;
+  final String author;
+  final String title;
+  final double price;
+  final Book bookobject;
+  @override
+  Widget build(BuildContext context) {
+   return GestureDetector(
+     onTap: (){},
+     child: Row(
+       children: <Widget>[
+         Column(
+           crossAxisAlignment: CrossAxisAlignment.start,
+           children: <Widget>[
+             Container(
+               height: 220,
+               width: 150,
+
+               decoration: BoxDecoration(
+                 borderRadius: BorderRadius.circular(5.0)
+               ),
+               child: ClipRRect(
+                 borderRadius: BorderRadius.circular(5.0),
+                 child: Image.network(
+                   bookCoverImg,
+                   fit: BoxFit.cover,
+                 ),
+               ),
+             ),
+             SizedBox(
+               height: 15,
+             ),
+             Text(
+               title,
+               style: TextStyle(
+                 fontWeight: FontWeight.w600,
+                 color: Colors.black87,
+                 fontSize: 20,
+
+               ),
+             ),
+             SizedBox(
+               height: 7,
+             ),
+             Text(
+               author,
+               style: TextStyle(
+                 fontWeight: FontWeight.w300,
+                 color: Colors.black45,
+                 fontSize: 15,
+
+               ),
+             ),
+             SizedBox(
+               height: 12,
+             ),
+             Text(
+               "\ Rs.$price",
+               style: TextStyle(
+                 fontWeight: FontWeight.w300,
+                 color: Colors.black45,
+                 fontSize: 15,
+
+               ),
+             ),
+             SizedBox(
+               height: 12,
+             ),
+
+
+           ],
+         ),
+         SizedBox(
+           width: 20,
+         ),
+       ],
+     ),
+   );
+  }
+}
+
